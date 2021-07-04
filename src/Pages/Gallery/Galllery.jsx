4e2e281591747e3da0354673042cd2308b7e462b
Saqlain1020 <saqlainprinters@gmail.com>
2021-07-04 20:22:29 +0500

    Image add btn in gallery

--- src/Pages/Gallery/Galllery.jsx
+++ src/Pages/Gallery/Galllery.jsx
@@ -1,10 +1,17 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core";
-import { Container } from "@material-ui/core";
+import { Container, Fab } from "@material-ui/core";
 import CustomGallery from "src/Components/CustomGallery/CustomGallery";
+import AddIcon from "@material-ui/icons/Add";
 
 const useStyles = makeStyles((theme) => ({
   root: { marginTop: 20 },
+  addIcon: {
+    position: "fixed",
+    right: 20,
+    bottom: 20,
+    color: "white",
+  },
 }));
 
 const data = [
@@ -38,11 +45,32 @@ const data = [
 ];
 const Galllery = () => {
   const classes = useStyles();
+  const ref = React.createRef();
+
+  const handleInputChange = (e) => {
+    //show image selected
+    console.log(e.target.files[0]);
+  };
 
   return (
     <div className={classes.root}>
       <Container maxWidth="lg">
         <CustomGallery data={data} />
+        <Fab
+          color="primary"
+          aria-label="add"
+          className={classes.addIcon}
+          onClick={() => ref.current.click()}
+        >
+          <AddIcon />
+        </Fab>
+        <input
+          type="file"
+          accept="image/*"
+          ref={ref}
+          style={{ display: "none" }}
+          onChange={handleInputChange}
+        />
       </Container>
     </div>
   );
